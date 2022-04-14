11bc92c4861df11cd37b7c331681c8ee5d90e9b6
Andrii Kotliar <andrijkotliar@gmail.com>
2022-04-14 15:02:49 +0300

    Change files extension

--- src/store/actions/modalActions.tsx
+++
@@ -1,9 +0,0 @@
-import { ModalActionTypes } from "../../types/modal";
-
-export const showModalAction = () => ({
-  type: ModalActionTypes.SHOW_MODAL
-})
-
-export const closeModalAction = () => ({
-  type: ModalActionTypes.HIDE_MODAL
-})
