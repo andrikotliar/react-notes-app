fa2c4dee9b9e91651b66acab4028fddeb289adbe
Andrii Kotliar <andrijkotliar@gmail.com>
2022-04-14 15:07:08 +0300

    Add modalFormReducer to rootReducer

--- src/store/reducers/reducers.ts
+++ src/store/reducers/reducers.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import { notesReducer } from "./notesReducer";
 import { modalReducer } from "./modalReducer";
+import { modalFormReducer } from "./modalFormReducer";
 
 export const rootReducer = combineReducers({
   notes: notesReducer,
-  modal: modalReducer
+  modal: modalReducer,
+  modalForm: modalFormReducer
 });
 
 export type RootState = ReturnType<typeof rootReducer>
