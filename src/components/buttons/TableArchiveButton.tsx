c6975301f5194bf2338b7795e47e42560780eca4
Andrii Kotliar <andrijkotliar@gmail.com>
2022-04-14 15:04:50 +0300

    Add TableArchiveButton component

---
+++ src/components/buttons/TableArchiveButton.tsx
@@ -0,0 +1,21 @@
+import { FC } from "react";
+import { useDispatch } from "react-redux";
+import { archiveNoteAction } from "../../store/actions/notesActions";
+
+import archiveIcon from "../../images/icons/archive-icon.svg";
+
+const TableArchiveButton : FC<{id: number}> = ({ id }) => {
+  const dispatch = useDispatch();
+
+  const onArchiveNote = (id: number) => {
+    dispatch(archiveNoteAction(id));
+  }
+
+  return (
+    <button className="table-row__button table-row__archive" onClick={ () => onArchiveNote(id)}>
+      <img src={archiveIcon} alt="" />
+    </button>
+  )
+}
+
+export default TableArchiveButton;
