5ad9419384259210030559d902380fd321d7b742
Andrii Kotliar <andrijkotliar@gmail.com>
2022-04-14 15:07:45 +0300

    Add body overflow styles

--- src/store/reducers/modalReducer.ts
+++ src/store/reducers/modalReducer.ts
@@ -7,11 +7,13 @@ const initialState : ModalState = {
 export const modalReducer = (state = initialState, action: ModalAction) : ModalState => {
   switch(action.type) {
     case ModalActionTypes.SHOW_MODAL:
+      document.body.style.overflow = "hidden";
       return {
         ...state,
         modal: true
       }
     case ModalActionTypes.HIDE_MODAL:
+      document.body.style.overflow = "";
       return initialState
     default:
       return state;
