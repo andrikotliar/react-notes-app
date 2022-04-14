308d6523d4dcd2f0404d50d0aa21c9f88da02614
Andrii Kotliar <andrijkotliar@gmail.com>
2022-04-14 15:08:50 +0300

    Update TableBody component

--- src/container/TableBody.tsx
+++ src/container/TableBody.tsx
@@ -1,13 +1,13 @@
 import { FC } from "react";
 import TableBodyRow from "../components/table/TableBodyRow";
-import { NoteType } from "../types/notes";
+import { TableBodyTypes } from "../types/tables";
 
 import "../styles/table/table-body.css";
 
-const TableBody: FC<{notes: NoteType[]}> = ({ notes }) => {
+const TableBody: FC<{data: any[], body: TableBodyTypes}> = ({ data, body }) => {
   return (
     <tbody className="table__body">
-      {notes.map(note => <TableBodyRow key={note.id} data={note} />)}
+      {data.map(note => <TableBodyRow key={note.id} data={note} structure={body} />)}
     </tbody>
   )
 }
