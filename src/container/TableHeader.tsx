5c4d8f5c0294ed272ba5e3cb37dfb3e4f6e12e01
Andrii Kotliar <andrijkotliar@gmail.com>
2022-04-14 22:04:00 +0300

    Add icons

--- src/container/TableHeader.tsx
+++ src/container/TableHeader.tsx
@@ -1,16 +1,19 @@
 import { FC } from "react";
 import { TableHeaderItem } from "../types/tables";
+
 import TableHeaderCell from "../components/table/TableHeaderCell";
+import TableHeaderIcons from "../components/table/TableHeaderIcons";
 
 import "../styles/table/table-header.css";
 
-const TableHeader: FC<{headers: TableHeaderItem[]}> = ({headers}) => {
+const TableHeader: FC<{headers: TableHeaderItem[], icons: boolean}> = ({headers, icons}) => {
   return (
     <thead className="table__header">
       <tr>
         {headers.map((header, index) =>
           <TableHeaderCell key={index} header={header} />
         )}
+        {icons && <TableHeaderIcons />}
       </tr>
     </thead>
   );
