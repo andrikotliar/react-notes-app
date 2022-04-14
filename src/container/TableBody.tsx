53ea7a5bb6cd01561e4ea5649cc43580915617cf
Andrii Kotliar <andrijkotliar@gmail.com>
2022-04-14 22:04:16 +0300

    Rename note to item

--- src/container/TableBody.tsx
+++ src/container/TableBody.tsx
@@ -5,9 +5,10 @@ import { TableBodyTypes } from "../types/tables";
 import "../styles/table/table-body.css";
 
 const TableBody: FC<{data: any[], body: TableBodyTypes}> = ({ data, body }) => {
+
   return (
     <tbody className="table__body">
-      {data.map(note => <TableBodyRow key={note.id} data={note} structure={body} />)}
+      {data.map(item => <TableBodyRow key={item.id} data={item} structure={body} />)}
     </tbody>
   )
 }
