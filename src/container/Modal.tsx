484de665072f20158e25f1675d804b7018c446c1
Andrii Kotliar <andrijkotliar@gmail.com>
2022-04-14 15:09:58 +0300

    Add ModalForm component

--- src/container/Modal.tsx
+++ src/container/Modal.tsx
@@ -1,7 +1,8 @@
-import { MouseEvent } from "react";
 import { useDispatch } from "react-redux";
 import { useTypedSelector } from "../hooks/useTypedSelector";
 import { closeModalAction } from "../store/actions/modalActions";
+
+import ModalForm from "../components/forms/ModalForm";
 
 import "../styles/modal/modal.css";
 
@@ -20,28 +21,9 @@ const Modal = () => {
   return (
     <div className="modal" id="modal">
       <div className="modal__overlay">
-        <div className="note-popup">
-          <input type="text" placeholder="Write note title..." className="note-popup__title" />
-          <h3>Choose a category</h3>
-          <div className="note-popup__categories">
-            <label className="note-popup__category">
-              <input type="radio" value="Task" name="category" className="note-category-input" checked={true} />
-              <span>Task</span>
-            </label>
-            <label className="note-popup__category">
-              <input type="radio" value="Idea" name="category" className="note-category-input" />
-              <span>Idea</span>
-            </label>
-            <label className="note-popup__category">
-              <input type="radio" value="Random Thought" name="category" className="note-category-input" />
-              <span>Random Thought</span>
-            </label>
-          </div>
-          <textarea className="note-popup__content" placeholder="Write note text..."></textarea>
-          <button id="save-note" className="note-popup__save save-note">
-            Save note
-          </button>
-          <button aria-label="Close modal" className="note-popup__close" onClick={onCloseModal}>
+        <div className="modal__content">
+          <ModalForm />
+          <button aria-label="Close the modal" className="modal__close" onClick={onCloseModal}>
             ✖
           </button>
         </div>
